9754211e2b5806b2106b6f4235e9c57d4bdbe465
Neil Sarkar <neil@groupme.com>
2012-10-14 15:20:08 -0400

    SSL on www.splitmeapp.com/api

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Splitme::Application.routes.draw do
-  constraints :subdomain => "api" do
-    scope :module => "api", :as => "api" do
-      resources :plans, only: [:index, :create, :show]
-      match "/plans/:id/collect/:participant_id" => "plans#collect", via: :post
-      match "/plans/:plan_token/join" => "plans#join", via: :post
-      match "/plans/:plan_token/preview" => "plans#preview"
+  namespace :api do
+    resources :plans, only: [:index, :create, :show]
+    match "/plans/:id/collect/:participant_id" => "plans#collect", via: :post
+    match "/plans/:plan_token/join" => "plans#join", via: :post
+    match "/plans/:plan_token/preview" => "plans#preview"
 
-      resources :users, only: [:create] do
-        post :collect, on: :member
-      end
-      match "/users/authenticate" => "users#authenticate", via: :post
-
-      match "/" => "hi#hi"
+    resources :users, only: [:create] do
+      post :collect, on: :member
     end
+    match "/users/authenticate" => "users#authenticate", via: :post
+
+    match "/" => "hi#hi"
   end
 
   mount JasmineRails::Engine => "/specs" if Rails.env.development?
