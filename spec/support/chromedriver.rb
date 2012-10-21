372ba8c5db8f1f4a0975e4b3027ee79e326edadb
Neil Sarkar <neil@groupme.com>
2012-10-21 13:14:47 -0400

    Add chromedriver check :check:

--- spec/support/chromedriver.rb
+++ spec/support/chromedriver.rb
@@ -1,3 +1,17 @@
+unless `which chromedriver`
+  warning = <<-END
+*******************************************************************************
+
+    you need to install chromedriver. go here:
+
+    http://code.google.com/p/chromedriver/downloads/list
+
+*******************************************************************************
+  END
+  puts warning
+  exit!
+end
+
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome, native_events: true)
 end
