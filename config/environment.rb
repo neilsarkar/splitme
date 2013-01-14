bfcc7e717a45eebf55b1a1447033bcb9698e7e11
Neil Sarkar <neil@groupme.com>
2013-01-14 14:14:29 -0500

    Sew up rails vulnerability :lock:
    Disable XML and YAML parsing to avoid rails exploit detailed here: https://github.com/ronin-ruby/ronin-ruby.github.com/blob/rails-pocs/blog/_posts/2013-01-09-rails-pocs.md

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::YAML)
+
 # Initialize the rails application
 Splitme::Application.initialize!
